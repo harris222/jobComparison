866093c4a08d3c72bf35c7d8e86ae85120dabcbb
Ethan Shen <ethan@MacBook-Pro.local>
2021-03-06 18:46:50 -0800

    text input and compare button draft

--- dash/src/pages/Ethan.js
+++ dash/src/pages/Ethan.js
@@ -1,7 +1,94 @@
-import React from "react";
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
 
-function App(){
-    return (<h1>Ethan</h1>);
+const profession1 = 'Profession';
+const profession2 = 'Profession';
+const description1 = 'Description';
+const description2 = 'Description';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    margin: `${theme.spacing(2)}px auto`,
+    padding: theme.spacing(3),
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Compare() {
+  const classes = useStyles();
+
+  return (
+    <Container component="main">
+      <CssBaseline />
+      <div className={classes.paper}>
+        
+        <Typography variant="h4" align="center">
+          Compare Required Skills
+        </Typography>
+
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                id="input-left"
+                label="Profession 1"
+                name="input-left"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                id="input-right"
+                label="Profession 2"
+                name="input-right"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            size="large"
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Compare
+          </Button>
+        </form>
+
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              <Typography variant="h6">{profession1}</Typography>
+              <Typography>{description1}</Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>
+              <Typography variant="h6">{profession2}</Typography>
+              <Typography>{description2}</Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+
+      </div>
+    </Container>
+  );
 }
-
-export default App;
